b9f4d057d9861ffa26200a7592b4c4bb3992247f
JScott <jvscott@gmail.com>
2015-01-27 10:38:34 -0800

    Small alias for future testing

--- js/third-party/cardboard/cardboard.js
+++ js/third-party/cardboard/cardboard.js
@@ -1,3 +1,32 @@
-function setCardboard
+function Cardboard(controls, element) {
+  this.controls = controls;
+  this.element = element;
+  
+  this.fullscreen = function() {
+    var container = element.parentNode;
 
-//window.addEventListener('deviceorientation', setOrientationControls, true);
+    if (container.requestFullscreen) {
+      container.requestFullscreen();
+    } else if (container.msRequestFullscreen) {
+      container.msRequestFullscreen();
+    } else if (container.mozRequestFullScreen) {
+      container.mozRequestFullScreen();
+    } else if (container.webkitRequestFullscreen) {
+      container.webkitRequestFullscreen();
+    }
+  };
+  
+  this.enable = function(e) {
+    if (e && !e.alpha) { // TODO: what is this?
+      return;
+    }
+
+    this.controls = new THREE.DeviceOrientationControls(camera, true);
+    this.controls.connect();
+    this.controls.update();
+
+    this.element.addEventListener('click', this.fullscreen(), false);
+
+    window.removeEventListener('deviceorientation', this.enable, true);
+  }
+}
